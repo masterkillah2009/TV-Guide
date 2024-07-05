b2b22c419bc142d1afc72a74b998ba4508734ef2
stormythunder4K <hamusutekevin@gmail.com>
2024-07-05 23:13:48 +0200

    Add files via upload

---
+++ TV Guide.py
@@ -0,0 +1,171 @@
+def tv_guide():
+    print("----WELCOME TO THE TV GUIDE----")
+    print("----CHOOSE A CHANNEL----")
+    print("1. XB News")
+    print("2. Blue Ocean")
+    print("3. SoundTrack")
+    print("4. Rebel Music")
+    print("5. Rosemary")
+    print("6. One Action")
+    print("7. Vue Sports X")
+    print("8. MovieMax More")
+    print("9. Foschia")
+    print("10. Mizu")
+    print("11. Rave TV")
+    print("12. Sparkle")
+    print("13. 13th Street")
+    channel_decision = input("> ")
+    if channel_decision == "1":
+        xb_news()
+        exit()
+    
+    elif channel_decision == "2":
+        blue_ocean()
+        exit()
+
+    elif channel_decision == "3":
+        SoundTrack()
+        exit()
+    
+    elif channel_decision == "4":
+        Rebel_Music()
+        exit()
+    
+    elif channel_decision == "5":
+        Rosemary()
+        exit()
+    
+    elif channel_decision == "6":
+        One_Action()
+        exit()
+    
+    elif channel_decision == "7":
+        Vue_Sports_X()
+        exit()
+    
+    elif channel_decision == "8":
+        MovieMax_More()
+        exit()
+    
+    elif channel_decision == "9":
+        Foschia()
+        exit()
+    
+    elif channel_decision == "10":
+        Mizu()
+        exit()
+    
+    elif channel_decision == "11":
+        Rave_TV()
+        exit()
+    
+    elif channel_decision == "12":
+        Sparkle()
+        exit()
+    
+    elif channel_decision == "13":
+        Thirteenth_Street()
+        exit()
+    
+    else:
+        print("Invalid Channel Number")
+        exit()
+
+def xb_news():
+    print("|----MONDAY, FEBRUARY 2, 2015----|")
+    print("08:45 pm - News at 8")
+    print("09:00 pm - The Asian Perspective")
+    print("09:30 pm - The Asian Perspective ")
+    print("10:00 pm - Byte This")
+
+def blue_ocean():
+    print("|----MONDAY, FEBRUARY 2, 2015----|")
+    print("08:35 pm - 60 Minutes Of Calm")
+    print("09:35 pm - A Night Of Serenity")
+    print("10:10 pm - Escape Into The Unknown")
+    print("10:45 pm - Lexi's Guide To Relaxation")
+
+def SoundTrack():
+    print("|----MONDAY, FEBRUARY 2, 2015----|")
+    print("08:20 pm - Top 10 Hits Of Today")
+    print("09:00 pm - Y2K Nirvana")
+    print("10:30 pm - 90s Nirvana")
+    print("11:45 pm - Electronic Party Blowout")
+
+def Rebel_Music():
+    print("|----MONDAY, FEBRUARY 2, 2015----|")
+    print("07:55 pm - Rock, Roll and Rhythm")
+    print("08: 45 pm - Top 25 Rocking Hits Of The 2000s")
+    print("09:55 pm - Old Country Roads")
+    print("10:50 pm - Nothing But Rhythm and Blues")
+
+def Rosemary():
+    print("|----MONDAY, FEBRUARY 2, 2015----|")
+    print("08:05 pm - Peaky Blinders")
+    print("08:55 pm - Boardwalk Empire")
+    print("09:45 pm - Suits")
+    print("10:45 pm - Downtown Abbey")
+
+def One_Action():
+    print("|----MONDAY, FEBRUARY 2, 2015----|")
+    print("08:05 pm - 24")
+    print("08:50 pm - Arrow")
+    print("09:55 pm - Hawaii Five-O")
+    print("10:50 pm - The Vampire Diaries")
+
+def Vue_Sports_X():
+    print("|----MONDAY, FEBRUARY 2, 2015----|")
+    print("08:25 pm - Depth Of Analysis")
+    print("08:55 pm - Barclays Premier League 14/15: Arsenal v Liverpool")
+    print("11:25 pm - LIGA BBVA 14/15: Barcelona v Real Madrid")
+    print("02:05 am - Australian Open")
+
+def MovieMax_More():
+    print("|----MONDAY, FEBRUARY 2, 2015----|")
+    print("08:00 pm - 300")
+    print("10:05 pm - Ender's Game")
+    print("11:55 pm - The Marine")
+    print("01:40 pm - Resident Evil")
+
+def Foschia():
+    print("|----MONDAY, FEBRUARY 2, 2015----|")
+    print("08:10 pm - Love Is In The Air")
+    print("09:00 pm - Valentine's Day")
+    print("10:45 pm - Desperate Housewives")
+    print("11:30 pm - Pacific Breeze")
+
+def Mizu():
+    print("|----MONDAY, FEBRUARY 2, 2015----|")
+    print("08:35 pm - Bleach")
+    print("09:00 pm - Sonic X")
+    print("09:25 pm - Dragonball Z")
+    print("09:50 pm - Yu-Gi-Oh!")
+
+def Rave_TV():
+    print("|----MONDAY, FEBRUARY 2, 2015----|")
+    print("08:30 pm - WWE Raw")
+    print("10:00 pm - Party Of Six")
+    print("11:00 pm - EuroVison 2014")
+    print("12:55 pm - Dubstep Central")
+
+def Sparkle():
+    print("|----MONDAY, FEBRUARY 2, 2015----|")
+    print("08:15 pm - Lolirock")
+    print("08:40 pm - My Little Pony")
+    print("09:05 pm - Peppa Pig")
+    print("09:20 pm - Sesame Street")
+
+def Thirteenth_Street():
+    print("|----MONDAY, FEBRUARY 2, 2015----|")
+    print("07:45 pm - The Big Bang Theory")
+    print("08:10 pm - Monsuno")
+    print("08:35 pm - The Simpsons")
+    print("09:00 pm - Buffy: The Vampire Slayer")
+
+
+
+
+    
+
+tv_guide()
+
